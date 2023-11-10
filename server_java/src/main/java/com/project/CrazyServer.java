a3dc9f59690855507f63c675c31beec24b902ef8
joelberzalgithub <120808626+joelberzalgithub@users.noreply.github.com>
2023-11-10 16:57:05 +0100

    Afegit un nou import

--- server_java/src/main/java/com/project/CrazyServer.java
+++ server_java/src/main/java/com/project/CrazyServer.java
@@ -2,6 +2,7 @@ package com.project;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
